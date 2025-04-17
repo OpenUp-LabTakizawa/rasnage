edcf891cfc31ae53cb1c75ab7fa2ed7898b377a0
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-04-17 10:45:03 +0900

    🧰chore: remove TURBOPACK=1 env (#804)

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "TURBOPACK=1 bun .next/standalone/server.js",
+    command: "bun .next/standalone/server.js",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
   },
