4248fff9ba1a23653ea3c3780316536a5b2fc337
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-05-22 19:23:01 +0900

    ♻️chore: test GitHub Actions (#835)

--- test/app/page.test.tsx
+++ test/app/page.test.tsx
@@ -8,6 +8,7 @@ test("Image test", () => {
   const img = document.querySelectorAll("img")
   expect(img[0]?.alt).toEqual("Next.js logo")
   expect(img[0]?.src).toEqual("/next.svg")
+  expect(img[0]?.width).toEqual(180)
   expect(img[1]?.alt).toEqual("Vercel logomark")
   expect(img[1]?.src).toEqual("/vercel.svg")
   expect(img[2]?.alt).toEqual("File icon")
